a1572ccba7c43e82c6dc5eb3f62ca9b5b7fd27f4
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-12 01:56:59 -0700

    Added employee present or absent

---
+++ Employeewageproblem/Employeecheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeewageproblem
+{
+    public static class Employeecheck
+    {
+        public static void  checkattendance()
+        {
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+            if(empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+
+            }
+        }
+    }
+}
