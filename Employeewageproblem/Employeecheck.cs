d36a8a7ad43d0d335b925076f24ce86dbe8ecaf1
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-14 16:53:19 +0530

    Added Employee absent or present or part time

--- Employeewageproblem/Employeecheck.cs
+++ Employeewageproblem/Employeecheck.cs
@@ -3,26 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Employeewageproblem
-{
-    public static class Employeecheck
+   
+namespace EmployeeWageproblem
     {
-        public static void  checkattendance()
+        public static class EmployeeCheck
         {
-            int IS_FULL_TIME = 1;
-
-            Random random = new Random();
-            int empcheck = random.Next(2);
-            if(empcheck == IS_FULL_TIME)
+            public static void Checkattendance()
             {
-                Console.WriteLine("Employee is present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
+                int IS_FULL_TIME = 1;
+                int IS_PART_TIME = 2;
+                int EMP_RATE_PER_HOUR = 20;
 
+                int empHrs = 0;
+                int empWage = 0;
+
+                Random random = new Random();
+                int empCheck = random.Next(3);
+                if (empCheck == IS_FULL_TIME)
+                {
+                    Console.WriteLine("Employee is full time");
+                    empHrs = 8;
+                }
+                else if (empCheck == IS_PART_TIME)
+                {
+                    Console.WriteLine("Employee is part time");
+                    empHrs = 4;
+                }
+
+                else
+                {
+                    Console.WriteLine("Employee is absent");
+                    empHrs = 0;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Emp Wage: " + empWage);
             }
         }
     }
-}
+
+
+  
